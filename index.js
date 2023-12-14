c35cbcc17d11fe88c69b1ab761c7e048a7a4ab78
Flyse <Belyaeva-Nastya-D@ya.ru>
2023-12-14 12:57:25 +0300

    fix cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import routes from './routes/routes.js';
-import dbController from './db/dbController.js'
-import cors from 'cors';
+import express from "express";
+import routes from "./routes/routes.js";
+import dbController from "./db/dbController.js";
+import cors from "cors";
 
 const PORT = 5000;
 const app = express();
@@ -9,22 +9,21 @@ const dataBase = new dbController();
 
 app.use(express.json());
 
-app.use(cors({
-    origin: (origin,func)=>{
-        if(!origin||origin === 'https://brullov-shop.neocities.org'){
-            func(null,true);
-        }else{
-            func(new Error('Доступ запрещён'));
-        }
+app.use(
+  cors({
+    origin: (origin, func) => {
+      if (!origin || origin === "https://brullov-shop.neocities.org") {
+        func(null, true);
+      } else {
+        func(new Error("Доступ запрещён"));
+      }
     },
-    methods: ['GET', 'POST'], 
-  }));
+    methods: ["GET", "POST"],
+  })
+);
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(PORT, () => console.log(`listening port ${PORT}`));
-
-
-
 
 export default dataBase;
